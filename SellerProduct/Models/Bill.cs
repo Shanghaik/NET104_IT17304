71dda367958b6b85e266a60155bb896d4ffd6b2b
shanghaik168@gmail.com <Shanghaik1998>
2023-03-11 10:12:56 +0700

    All Models

--- SellerProduct/Models/Bill.cs
+++ SellerProduct/Models/Bill.cs
@@ -7,6 +7,6 @@
         public Guid UserId { get; set;}
         public int Status { get; set; }
         public virtual ICollection<BillDetails> BillDetails { get;set; }
-
+        public virtual User User { get; set; }
     }
 }
