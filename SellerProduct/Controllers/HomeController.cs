88cd52786542d062edf03767394b4ebc239323bf
shanghaik168@gmail.com <Shanghaik1998>
2023-03-16 11:10:02 +0700

    View

--- SellerProduct/Controllers/HomeController.cs
+++ SellerProduct/Controllers/HomeController.cs
@@ -15,12 +15,64 @@ namespace SellerProduct.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(); // Trả về - hiển thị view Cùng tên với Action
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Test");   // Chuyển hướng về action Test
+        }
+        // truyền dữ liệu từ Action qua View
+        public ActionResult Show()
+        {
+            Product product = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "+ Ngọc Bảo",
+                Description = "Học lại",
+                Supplier = "Mama Bank",
+                Price = 672000,
+                AvailableQuantity = 1,
+                Status = 1
+            };
+            return View(product); // Truyền trực tiếp 1 Obj Model duy nhất sang View
+
+        }
+        public ActionResult ShowAllProduct()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product()
+                {
+                Id = Guid.NewGuid(),
+                Name = "+ Ngọc Bảo",
+                Description = "Học lại",
+                Supplier = "Mama Bank",
+                Price = 672000,
+                AvailableQuantity = 1,
+                Status = 1
+                },
+                new Product()
+                {
+                Id = Guid.NewGuid(),
+                Name = "- Ngọc Bảo",
+                Description = "Vẫn là học lại nhưng bị lập biên bản",
+                Supplier = "Thầy Khánh",
+                Price = 672000 * 4,
+                AvailableQuantity = 2,
+                Status = 0
+                }
+            };
+            return View(products); // Truyền trực tiếp 1 Obj Model duy nhất sang View
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
